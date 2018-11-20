d932e492cedea16ea084bd33fedba87222b688b4
Alexander Todorov <alext@Alexanders-MacBook-Pro.local>
2018-11-20 15:29:41 +0200

    updated kitchen tests

--- test/integration/default/check_ip.rb
+++ test/integration/default/check_ip.rb
@@ -3,7 +3,7 @@ external_ip = attribute(
   description: "The Terraform configuration under test must define an "
 )
 
-control 'check_string' do
+control 'check_ip' do
   describe external_ip do 
     it { should_not eq "nil" } 
     it { should_not eq "" }
